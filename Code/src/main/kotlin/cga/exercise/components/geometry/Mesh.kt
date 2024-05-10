21061b18e642be2760f360c5bf6bd7f99fb809ed
danyaconigriega <dgomezc2@itam.mx>
2024-05-10 14:43:22 +0200

    Added Assignments 1.1 and 1.2.

--- Code/src/main/kotlin/cga/exercise/components/geometry/Mesh.kt
+++ Code/src/main/kotlin/cga/exercise/components/geometry/Mesh.kt
@@ -1,40 +1,70 @@
 package cga.exercise.components.geometry
 
+import cga.exercise.components.shader.ShaderProgram
 import org.lwjgl.opengl.GL30.*
 
-/**
- * Creates a Mesh object from vertexdata, intexdata and a given set of vertex attributes
- *
- * @param vertexdata plain float array of vertex data
- * @param indexdata  index data
- * @param attributes vertex attributes contained in vertex data
- * @throws Exception If the creation of the required OpenGL objects fails, an exception is thrown
- *
- * Created by Fabian on 16.09.2017.
- */
 class Mesh(vertexdata: FloatArray, indexdata: IntArray, attributes: Array<VertexAttribute>) {
-    //private data
-    private var vaoId = 0
-    private var vboId = 0
-    private var iboId = 0
-    private var indexcount = 0
+    // OpenGL identifiers for vertex array, vertex buffer, and index buffer objects
+    private var vaoId: Int = glGenVertexArrays()
+    private var vboId: Int = glGenBuffers()
+    private var iboId: Int = glGenBuffers()
+    private var indexCount: Int = indexdata.size
 
     init {
-        // ToDo
-        // Aufgabe 1.2.2
-        // shovel geometry data to GPU and tell OpenGL how to interpret it
+        // 1. Initialize and Bind VAO: Setup a new VAO and bind it to configure VBO and IBO
+        glBindVertexArray(vaoId)
+
+        // 2. Initialize and Bind VBO: Setup a new VBO, bind it, and fill it with vertex data
+        glBindBuffer(GL_ARRAY_BUFFER, vboId)
+        glBufferData(GL_ARRAY_BUFFER, vertexdata, GL_STATIC_DRAW)
+
+        // 3. Configure Vertex Attributes: Specify the layout of the vertex data
+        for (i in attributes.indices) {
+            glEnableVertexAttribArray(i) // Enable the vertex attribute at index 'i'
+            glVertexAttribPointer(
+                i, // Attribute index
+                attributes[i].n, // Number of components per vertex attribute
+                attributes[i].type, // Data type of each component
+                false, // Normalization
+                attributes[i].stride, // Byte offset between consecutive attributes
+                attributes[i].offset // Byte offset to the first component
+            )
+        }
+
+        // 4. Initialize and Bind IBO: Setup a new IBO, bind it, and fill it with index data
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexdata, GL_STATIC_DRAW)
+
+        // 5. Unbind VAO: Unbind the VAO to avoid accidental modification
+        glBindVertexArray(0)
     }
 
-    //Only send the geometry to the gpu
     /**
-     * renders the mesh
+     * Renders the mesh using the specified shader program.
+     * - Activates the shader program
+     * - Binds the VAO to setup the GPU for rendering
+     * - Draws the elements based on index data
+     * - Unbinds the VAO to clean up
+     */
+    fun render(shaderProgram: ShaderProgram) {
+        shaderProgram.use() // Activate the shader program
+        glBindVertexArray(vaoId) // Bind the VAO
+        glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0) // Draw the elements
+        glBindVertexArray(0) // Unbind the VAO
+    }
+
+    /**
+     * Renders the mesh assuming the shader is already activated and bound.
      */
     fun render() {
-        // ToDo
+        glBindVertexArray(vaoId)
+        glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0)
+        glBindVertexArray(0)
     }
 
     /**
-     * Deletes the previously allocated OpenGL objects for this mesh
+     * Cleans up the allocated resources.
+     * - Deletes VAO, VBO, and IBO if they were created.
      */
     fun cleanup() {
         if (vboId != 0) glDeleteBuffers(vboId)
--- Code/src/main/kotlin/cga/exercise/game/Scene.kt
+++ Code/src/main/kotlin/cga/exercise/game/Scene.kt
@@ -5,7 +5,6 @@ import cga.exercise.components.geometry.VertexAttribute
 import cga.exercise.components.shader.ShaderProgram
 import cga.framework.GLError
 import cga.framework.GameWindow
-import cga.framework.OBJLoader.loadOBJ
 import org.lwjgl.opengl.GL11.*
 
 /**
@@ -14,44 +13,91 @@ import org.lwjgl.opengl.GL11.*
 class Scene(private val window: GameWindow) {
     private val staticShader: ShaderProgram = ShaderProgram("assets/shaders/simple_vert.glsl", "assets/shaders/simple_frag.glsl")
 
-    //private val simpleMesh: Mesh
-
+    private lateinit var mesh1: Mesh  // Mesh now properly initialized
+    private lateinit var mesh2: Mesh  // This is another Mesh we'll use for our Initials
 
     //scene setup
     init {
-        val vertices = floatArrayOf(
-            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-            0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-            0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-            0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-            -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f
+        val vertices1 = floatArrayOf(
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // Bottom left blue
+            0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // Bottom right blue
+            0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // Top right green
+            0.0f,  1.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // Tip top red
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f   // Top left green
         )
 
-        val indices = intArrayOf(
+        val indices1 = intArrayOf(
+            0, 1, 2,  // Main triangle base
+            0, 2, 4,  // Left side
+            2, 3, 4   // Roof
+        )
+
+        val attributes1 = arrayOf(
+            VertexAttribute(3, GL_FLOAT, 24, 0L),   // Position attribute
+            VertexAttribute(3, GL_FLOAT, 24, 12L)   // Color attribute
+        )
+
+        // Mesh initialization using provided vertices, indices, and attributes
+        mesh1 = Mesh(vertices1, indices1, attributes1)
+
+        val vertices2 = floatArrayOf(
+            -0.75f, 0.25f, 0.0f, 0.0f, 0.0f, 1.0f,
+            -0.75f, -0.25f, 0.0f, 0.0f, 0.0f, 1.0f,
+            -0.5f,  -0.25f, 0.0f, 0.0f, 1.0f, 0.0f,
+            -0.25f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+            -0.5f,  0.25f, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.25f, 0.25f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.25f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.25f,  -0.25f, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.75f,  -0.25f, 0.0f, 1.0f, 0.0f, 0.0f,
+            0.75f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+            0.75f,  0.25f, 0.0f, 0.0f, 1.0f, 0.0f,
+            0.5f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f
+        )
+
+        val indices2 = intArrayOf(
             0, 1, 2,
             0, 2, 4,
-            4, 2, 3
+            2, 3, 4,
+            5, 6, 10,
+            6, 7, 8,
+            8, 9, 11
         )
 
-        // ToDo
+        val attributes2 = arrayOf(
+            VertexAttribute(3, GL_FLOAT, 24, 0L),   // Position attribute
+            VertexAttribute(3, GL_FLOAT, 24, 12L)   // Color attribute
+        )
 
-        //initial opengl state
-        glClearColor(0.0f, 0.533f, 1.0f, 1.0f); GLError.checkThrow()
+        // Mesh initialization using provided vertices, indices, and attributes
+        mesh2 = Mesh(vertices2, indices2, attributes2)
 
+        // Set initial OpenGL state
+        glClearColor(0.0f, 0.533f, 1.0f, 1.0f)
+        GLError.checkThrow()
     }
 
     fun render(dt: Float, t: Float) {
-        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
-        staticShader.use()
-
-        // ToDo
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT) // Clear the frame buffer
+        staticShader.use() // Use the static shader program
+        // mesh1.render() // Render the mesh
+        mesh2.render() // Render the mesh
     }
 
-    fun update(dt: Float, t: Float) {}
+    fun update(dt: Float, t: Float) {
+        // Update logic can be added here if necessary
+    }
 
-    fun onKey(key: Int, scancode: Int, action: Int, mode: Int) {}
+    fun onKey(key: Int, scancode: Int, action: Int, mode: Int) {
+        // Keyboard input handling can be implemented here
+    }
 
-    fun onMouseMove(xpos: Double, ypos: Double) {}
+    fun onMouseMove(xpos: Double, ypos: Double) {
+        // Mouse movement handling can be implemented here
+    }
 
-    fun cleanup() {}
+    fun cleanup() {
+        mesh2.cleanup() // Clean up the mesh resources
+        staticShader.cleanup() // Clean up the shader resources
+    }
 }
